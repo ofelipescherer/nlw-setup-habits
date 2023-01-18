5dafc194d9c170742cad393f316b70c2999343de
Felipe Scherer <felipescherer25@gmail.com>
2023-01-18 20:33:23 -0300

    :sparkles: feat: backend routes

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const firstHabitId = "0730ffac-d039-4194-9571-01aa2aa0efbd";
-const firstHabitCreationDate = new Date("2022-12-31T03:00:00.000");
+const firstHabitCreationDate = new Date("2022-12-31T14:00:00.000");
 
 const secondHabitId = "00880d75-a933-4fef-94ab-e05744435297";
 const secondHabitCreationDate = new Date("2023-01-03T03:00:00.000");
@@ -11,9 +11,14 @@ const secondHabitCreationDate = new Date("2023-01-03T03:00:00.000");
 const thirdHabitId = "fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00";
 const thirdHabitCreationDate = new Date("2023-01-08T03:00:00.000");
 
+const forthHabitId = "ta1a1bcf-3d87-4626-8c0d-d7fd1255ac01";
+const forthHabitCreationDate = new Date("2023-01-08T03:00:00.000");
+
 async function run() {
-  await prisma.habit.deleteMany();
+  await prisma.habitWeekDays.deleteMany();
+  await prisma.dayHabit.deleteMany();
   await prisma.day.deleteMany();
+  await prisma.habit.deleteMany();
 
   /**
    * Create habits
@@ -48,6 +53,24 @@ async function run() {
         created_at: thirdHabitCreationDate,
         weekDays: {
           create: [
+            { week_day: 1 },
+            { week_day: 2 },
+            { week_day: 3 },
+            { week_day: 4 },
+            { week_day: 5 },
+          ],
+        },
+      },
+    }),
+
+    prisma.habit.create({
+      data: {
+        id: forthHabitId,
+        title: "JOGAR VIDEOGAME",
+        created_at: forthHabitCreationDate,
+        weekDays: {
+          create: [
+            { week_day: 0 },
             { week_day: 1 },
             { week_day: 2 },
             { week_day: 3 },
