c4f87b6d483dc184ac7694d839cd4357e1836663
Felipe Scherer <felipescherer25@gmail.com>
2023-01-18 22:04:18 -0300

    :sparkles: feat: habit popover and modal

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,29 @@
+import * as Progress from '@radix-ui/react-progress';
+
+type ProgressBarProps = {
+  progress: number;
+};
+
+export function ProgressBar(props: ProgressBarProps) {
+  return (
+    <>
+      {/* Barra de progresso acessível */}
+      {/* <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+            <div
+              role="progressbar"
+              aria-label="Progresso de hábitos completados nesse dia"
+              aria-value={75}
+              className="h-3 rounded-xl bg-violet-600 w-3/4"
+            ></div>
+          </div> */}
+
+      {/* Usando o Radix para fazer uma barra de progresso acessível também */}
+      <Progress.Root className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+        <Progress.Indicator
+          className="h-3 rounded-xl bg-violet-600"
+          style={{ width: `${props.progress}%` }}
+        ></Progress.Indicator>
+      </Progress.Root>
+    </>
+  );
+}
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -24,7 +24,11 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDate.map(date => (
-          <HabitDay key={date.toString()} />
+          <HabitDay
+            key={date.toString()}
+            amount={16}
+            completed={Math.round(Math.random() * 16)}
+          />
         ))}
 
         {fillDates > 0 &&
